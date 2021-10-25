fb08564ff83495d550d91c14a696edd5ed74de4e
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-10-25 18:43:17 +0700

    Learn Poincut Declarations

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,12 +2,16 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
 //	@Before("execution(public void addAccount())")
 //	public void beforeAddAccountAdvice() {
 //		System.out.println("\n====>>> Executing @Before advice on addAccount()");
@@ -45,7 +49,7 @@ public class MyDemoLoggingAspect {
 //	}
 	
 	
-	@Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	@Before("forDaoPackage()")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n====>>> Executing @Before advice on addAccount()");
 	}
